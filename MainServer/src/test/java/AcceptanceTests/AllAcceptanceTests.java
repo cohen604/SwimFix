e6111a424452086b19507e0a4e4788d3a4fd2f8a
avraham101 <avrahamcalev@gmail.com>
2020-12-04 13:58:24 +0200

    Added more test folders

---
+++ MainServer/src/test/java/AcceptanceTests/AllAcceptanceTests.java
@@ -0,0 +1,13 @@
+package AcceptanceTests;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+
+})
+
+public class AllAcceptanceTests {
+
+}
