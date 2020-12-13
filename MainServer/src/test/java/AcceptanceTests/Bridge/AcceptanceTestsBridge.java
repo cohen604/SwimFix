c3d22929d4837d4ee18b584f92fbe78c5cd37fe0
avraham101 <avrahamcalev@gmail.com>
2020-12-13 14:58:26 +0200

    Unit Tests: VideoHandler #10

--- MainServer/src/test/java/AcceptanceTests/Bridge/AcceptanceTestsBridge.java
+++ MainServer/src/test/java/AcceptanceTests/Bridge/AcceptanceTestsBridge.java
@@ -6,6 +6,8 @@ import DTO.FeedbackVideoDTO;
 import DTO.FeedbackVideoStreamer;
 
 public interface AcceptanceTestsBridge {
+    //here need to be all the function from the Swimfix api
     ActionResult<FeedbackVideoStreamer> uploadVideoForStreamer(ConvertedVideoDTO convertedVideoDTO);
     ActionResult<FeedbackVideoDTO> streamFile(String path);
+
 }
