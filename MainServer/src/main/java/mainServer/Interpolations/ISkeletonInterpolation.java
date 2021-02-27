5130327e813fe15e7c62a219d6d28d5af050bdd8
avraham101 <avrahamcalev@gmail.com>
2021-02-27 20:00:32 +0200

    Added documentation for the interfaces

--- MainServer/src/main/java/mainServer/Interpolations/ISkeletonInterpolation.java
+++ MainServer/src/main/java/mainServer/Interpolations/ISkeletonInterpolation.java
@@ -4,8 +4,16 @@ import Domain.SwimmingData.ISwimmingSkeleton;
 
 import java.util.List;
 
+/**
+ * This interface defines how to interpolate over list of swimming skeletons
+ */
 public interface ISkeletonInterpolation {
 
+    /***
+     * The function receives a list of skeletons and interpolate over them
+     * @param skeletons - the skeletons
+     * @return the new swimming skeletons created after the interpolation
+     */
     List<ISwimmingSkeleton> interpolate(List<ISwimmingSkeleton> skeletons);
 
 }
--- MainServer/src/main/java/mainServer/Interpolations/Interpolation.java
+++ MainServer/src/main/java/mainServer/Interpolations/Interpolation.java
@@ -4,7 +4,15 @@ import Domain.SwimmingData.Points.IPoint;
 
 import java.util.List;
 
+/***
+ *  the interface that defines the interpolation behavior.
+ */
 public interface Interpolation {
 
+    /***
+     * The function receives a list of points and interpolate over them
+     * @param points - the points
+     * @return the new points after the interpolate
+     */
     List<IPoint> interpolate(List<IPoint> points);
 }
