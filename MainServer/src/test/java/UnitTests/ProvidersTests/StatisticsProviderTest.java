4a4d1a7e4dd0e825e6ffee5b3894e21fbc859ccb
avraham101 <avrahamcalev@gmail.com>
2021-03-21 12:04:16 +0200

    commit before pull request

--- MainServer/src/test/java/UnitTests/ProvidersTests/StatisticsProviderTest.java
+++ MainServer/src/test/java/UnitTests/ProvidersTests/StatisticsProviderTest.java
@@ -3,19 +3,37 @@ package UnitTests.ProvidersTests;
 import DTO.FileDTO;
 import junit.framework.TestCase;
 import mainServer.Providers.StatisticsProvider;
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
 
 public class StatisticsProviderTest extends TestCase {
 
     private StatisticsProvider _statisticsProvider;
+    private List<String> deleteList;
 
     @Before
     public void setUp() {
         _statisticsProvider = new StatisticsProvider();
+        deleteList = new LinkedList<>();
+    }
+
+    @After
+    public void tearDown() {
+        for(String path: deleteList) {
+            try {
+                Files.delete(Paths.get(path));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public void testGetStatistics() {
@@ -27,8 +45,10 @@ public class StatisticsProviderTest extends TestCase {
             byte[] bytes = Files.readAllBytes(Paths.get(rawPath));
             FileDTO fileDTO = new FileDTO(skeletonsPath, bytes);
             //Act
-            _statisticsProvider.getStatistics(fileDTO, skeletonsPath, pdfFolder );
+            String filePath = _statisticsProvider.getStatistics(fileDTO, skeletonsPath, pdfFolder );
+            deleteList.add(filePath);
             //Assert
+            Assert.assertTrue(Files.exists(Paths.get(filePath)));
         } catch (Exception e) {
             e.printStackTrace();
             fail();
