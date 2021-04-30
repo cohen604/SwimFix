2b8c5ad6e78da52975d46f82b7b978db44bb2fcb
avraham101 <avrahamcalev@gmail.com>
2021-04-30 21:43:01 +0300

    Finished IPointUtilsTest Unit and Integration tests

--- MainServer/src/test/java/IntegrationTests/AllIntegrationTests.java
+++ MainServer/src/test/java/IntegrationTests/AllIntegrationTests.java
@@ -1,11 +1,12 @@
 package IntegrationTests;
+import IntegrationTests.PointsTests.PointsTests;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 
 @Suite.SuiteClasses({
-
+    PointsTests.class,
 })
 
 public class AllIntegrationTests {
